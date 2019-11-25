9767ac082a63264710dc28c4de2055f2913a6674
Tim Arenz <tim@arenz.cc>
2019-11-25 15:20:03 +0100

    Adding core infrastructure

---
+++ enterprise/core/main.tf
@@ -0,0 +1,13 @@
+provider "azurerm" {}
+
+module "vnet" {
+  source           = "app.terraform.io/timarenz/environment/azurerm"
+  version          = "0.2.0"
+  environment_name = var.environment
+  owner_name       = var.owner
+  region           = "West Europe"
+  subnets = [{
+    name   = "subnet-1"
+    prefix = "192.168.42.0/24"
+  }]
+}
---
+++ enterprise/core/outputs.tf
@@ -0,0 +1,11 @@
+output "rg_name" {
+  value = module.vnet.rg_name
+}
+
+output "rg_location" {
+  value = module.vnet.rg_location
+}
+
+output "subnet_ids" {
+  value = module.vnet.subnet_ids
+}
