8b2bb9324e15d8b1b5425b6197c2ad9987d144e7
pschulz1 <50155921+pschulz1@users.noreply.github.com>
2019-11-29 20:35:54 +0100

    Update main.tf

--- enterprise/core/main.tf
+++ enterprise/core/main.tf
@@ -11,3 +11,15 @@ module "vnet" {
     prefix = "192.168.42.0/24"
   }]
 }
+
+  module "vnet2" {
+  source           = "app.terraform.io/timarenz/environment/azurerm"
+  version          = "0.2.0"
+  environment_name = var.environment
+  owner_name       = var.owner
+  region           = "West Europe"
+  subnets = [{
+    name   = "subnet-2"
+    prefix = "192.168.43.0/24"
+  }]
+}
